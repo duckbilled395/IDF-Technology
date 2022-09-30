2918258c5aa274dd3007a2c3b20ff86c1df84cda
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-09-30 19:51:24 +0300

    Delete src directory

--- src/shared/components/BreadcrumbsBox/lib.ts
+++
@@ -1,6 +0,0 @@
-import { AppDispatch } from '../../store'
-import { setNextStepType } from '../../store/signUp/types'
-
-export const handleOnClickSignUpInfo = (dispatch: AppDispatch, setNextStep: (order: number) => setNextStepType) => {
-	dispatch(setNextStep(1))
-}
